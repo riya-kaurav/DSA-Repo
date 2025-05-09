1b85e37e8b29ad7c492c95d59314e6fa94842733
riya-kaurav <kauravriya665@gmail.com>
2025-05-09 09:47:14 -0700

    adding new files

---
+++ pointer3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int arr[10] ={23, 122, 41, 67};
+   
+    cout << "address of first element: " << arr << endl; // address of first element
+
+cout<< *arr << endl; // value of first element
+cout << *arr + 1 << endl; // value of first element + 1
+cout << *(arr + 1) << endl; // value of second element
+int i =3;
+cout <<i[arr] << endl; // value of third element
+
+
+
+
+} 

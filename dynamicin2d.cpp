ed050610de2a6c1681fb4514cccfc343aa201dd0
riya-kaurav <kauravriya665@gmail.com>
2025-05-11 04:50:43 -0700

    adding files

--- dynamicin2d.cpp
+++ dynamicin2d.cpp
@@ -3,14 +3,32 @@ using namespace std;
 
 int main(){
 
+  int n ;
+  cin>> n;
+  int** arr = new int*[n];
+  for(int i =0; i<n ; i++){
+    arr[i] = new int[n];
+
+    //creation of 2d array done
+  }
+
+  for(int i =0; i<n ; i++){
+  
+for(int j =0; j<n ; j++){
+    cin >> arr[i][j];
+
+}
+  }
+
+  // how to release memory 
+for(int i =0; i< n; i++){
+    delete [] arr[i];
+
+
+}
+ delete []arr;
+ 
 
 
 
-
-
-
-
-
-
-    
 }
